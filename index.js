ed8488a76fded7cdc8e12f00a88d1da109bba013
rummanhase <rummanhase@gmail.com>
2023-04-10 11:44:27 +0530

    completed users routesAPI

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('./dependencies');
 const userRoute = require('./routers/user.routes')
+const PORT = 4000;
 
 const app = express();
 app.use(express.json());
@@ -7,6 +8,6 @@ app.use(express.json());
 
 app.use("/" , userRoute);
 
-app.listen(4000 , ()=>{
-    console.log("App is listening at 4000");
+app.listen( PORT, ()=>{
+    console.log("App is listening at "+PORT);
 })
