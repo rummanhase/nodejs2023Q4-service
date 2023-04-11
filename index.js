3bfe6cf9a3baf74d2fda4e5a8c992f10c194ce65
rummanhase <rummanhase@gmail.com>
2023-04-11 15:50:24 +0530

    added album base route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('./dependencies');
 const userRoute = require('./routers/user.routes');
 const trackRoute = require('./routers/track.routes');
 const artistRoute = require('./routers/artist.routes');
+const albumRoute = require('./routers/albums.routes')
 const PORT = 4000;
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use("/" , userRoute);
 app.use("/" , trackRoute)
 app.use('/' , artistRoute)
+app.use("/" , albumRoute)
 
 app.listen( PORT, ()=>{
     console.log("App is listening at "+PORT);
