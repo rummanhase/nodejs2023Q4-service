5eb1f884403179229201fa5458b468d35ad5f252
rummanhase <rummanhase@gmail.com>
2023-04-12 13:30:14 +0530

    final of users route with all possible conditon

--- buissenessLogics/user.logic.js
+++ buissenessLogics/user.logic.js
@@ -1,4 +1,12 @@
-let user = []
+let user = [
+    {
+        id: 1, // uuid v4
+        login: 'Rumman',
+        password: 'rumman@123',
+        version: 1, 
+        createdAt: new Date(), 
+        }
+]
 
 
 module.exports = {
